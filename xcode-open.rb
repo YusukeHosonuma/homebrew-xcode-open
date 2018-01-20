8f4d54c44f44c5a62a49da81afccff1c3c2109ce
Yusuke Hosonuma <tobi462@gmail.com>
2018-01-20 23:38:26 +0900

    Update version to 0.1.6

--- xcode-open.rb
+++ xcode-open.rb
@@ -1,8 +1,8 @@
 class XcodeOpen < Formula
   desc "Support open Xcode in terminal (with specify version)"
   homepage ""
-  url "https://github.com/YusukeHosonuma/xcode-open/archive/0.1.5.tar.gz"
-  sha256 "a5bd440c76b9274f4849a4fad35aca30a593786b175ca2765735cfa65b27177f"
+  url "https://github.com/YusukeHosonuma/xcode-open/archive/0.1.6.tar.gz"
+  sha256 "df611e06ebe8f8c99353a2760cd88439a575239545479e6f667e9e580ffb3d1a"
 
   def install
     system "make", "release-build"
