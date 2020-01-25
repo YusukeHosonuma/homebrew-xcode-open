41516b1d1948909f4253605bd53727550db5de99
Yusuke Hosonuma <tobi462@gmail.com>
2020-01-25 19:19:44 +0900

    Update version to 1.2.0

--- xcode-open.rb
+++ xcode-open.rb
@@ -1,13 +1,13 @@
 class XcodeOpen < Formula
   desc "Support open Xcode in terminal (with specify version)"
   homepage ""
-  url "https://github.com/YusukeHosonuma/xcode-open/archive/1.1.0.tar.gz"
-  sha256 "6cd3224f586de637743cc9146adf1efe3d7f21d230188b5497a4402ea6d5f002"
+  url "https://github.com/YusukeHosonuma/xcode-open/archive/1.2.0.tar.gz"
+  sha256 "b08a9baca233133dfdf90961946e93e4f495388ba6318ddcedc98a96c9b6dd55"
 
   def install
     system "make", "release-build"
     bin.install ".build/release/xcode-open"
   end
 
-  depends_on :xcode => ["9.0", :build]
+  depends_on :xcode => ["11.2", :build]
 end
